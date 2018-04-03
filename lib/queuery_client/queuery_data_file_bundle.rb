c9a84dc6a3e4d3b9d52524c5c4b71cd988e623d2
Hidekazu Kobayashi <hidekazu-kobayashi@cookpad.com>
2018-04-03 17:46:07 +0900

    Implement direct access method to S3 instead of via presigned URL

--- lib/queuery_client/queuery_data_file_bundle.rb
+++ lib/queuery_client/queuery_data_file_bundle.rb
@@ -1,4 +1,7 @@
 require 'redshift_connector/url_data_file_bundle'
+require 'redshift_connector/s3_bucket'
+require 'redshift_connector/s3_data_file_bundle'
+require 'uri'
 
 module QueueryClient
   class QueueryDataFileBundle < RedshiftConnector::UrlDataFileBundle
@@ -14,6 +17,14 @@ module QueueryClient
       uri.query = nil
       uri.path = File.dirname(uri.path)
       uri.to_s
+    end
+
+    def direct(bucket_opts = {}, bundle_opts = {})
+      s3_uri = URI.parse(s3_prefix)
+      bucket_name = s3_uri.host
+      prefix = s3_uri.path[1..-1] # trim heading slash
+      bucket = RedshiftConnector::S3Bucket.new(bucket: bucket_name, prefix: prefix)
+      RedshiftConnector::S3DataFileBundle.new(bucket, prefix, format: :redshift_csv)
     end
   end
 end
