ee063209a45623d0591872657de7ebf228ed674c
Ichinari Sato <ichinari-sato@cookpad.com>
2021-08-31 17:07:12 +0900

    Cache column_type to instance variable in Manifest subclass

--- lib/queuery_client/manifest_file.rb
+++ lib/queuery_client/manifest_file.rb
@@ -7,13 +7,14 @@ module QueueryClient
     end
 
     def column_types
-      f = open
-      begin
-        j = JSON.load(f)
-        j['schema']['elements'].map{|x| x['type']['base']}
-      ensure
-        f.close
-      end
+      @column_types ||=
+        begin
+          f = open
+          j = JSON.load(f)
+          j['schema']['elements'].map{|x| x['type']['base']}
+        ensure
+          f.close
+        end
     end
   end
 end
